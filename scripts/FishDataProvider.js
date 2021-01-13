1a1f72c0df25b76c824e79255697f5f3dab56a50
dtracydepp <tracydepp82@gmail.com>
2021-01-13 09:54:32 -0600

    created objects to rep fish,imported function to main js

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,59 @@
+// maintains the state of each fish
+
+
+const fishCollection = [
+    {
+        name: "Scar",
+        species: "Butterfly Fish",
+        location: "Coral reefs in the Atlantic, Indian and Pacific Oceans",
+        size: 9,
+        diet: " Zooplankton, tiny motile crustaceans and mollusks, and types of soft and stony corals",
+        image:"https://www.fishkeepingworld.com/wp-content/uploads/2018/12/Threadfin-Butterflyfish.jpg"
+
+    },
+
+    {
+        name: "Lilo",
+        species: "Discus",
+        location: "Amazon River",
+        size: 6,
+        diet: "Algae flakes, tropical flakes, and shrimp pellets",
+        image: "https://www.petlandtexas.com/wp-content/uploads/2020/01/Petland_Texas_Discus.jpg"
+
+    },
+
+    {
+        name: "Stitch",
+        species: "Flowerhorn Cichlid",
+        location: "Singapore and Malaysia",
+        size: 11,
+        diet: "Crickets, mealworms, and frozen shrimp",
+        image: "https://www.petlandtexas.com/wp-content/uploads/2020/01/Petland_Texas_Flowerhorn_Cichlid.jpg"
+
+    },
+
+    {
+        name: "Nyla",
+        species: "Flowerhorn Cichlid",
+        location: "Singapore and Malaysia",
+        size: 3,
+        diet: "Crickets, mealworms, and frozen shrimp",
+        image:"https://www.petlandtexas.com/wp-content/uploads/2020/01/Petland_Texas_Flowerhorn_Cichlid.jpg"
+
+    },
+
+    {
+        name: "Gank",
+        species: "Discus",
+        location: "Amazon River",
+        size: 16,
+        diet: "Algae flakes, tropical flakes, and shrimp pellets",
+        image: "https://www.fishkeepingworld.com/wp-content/uploads/2018/12/Threadfin-Butterflyfish.jpg"
+
+    }
+]
+
+export const useFish = () => {
+    return fishCollection.slice()
+}
+console.log (fishCollection)
